6f45382886689b6164d8f7dd59c3e042dbdbfbc1
JuanSoriano <ju_soriano88@hotmail.com>
2019-05-12 00:56:44 -0300

    tp2 mod

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -13,13 +13,31 @@
 /* Lectura de una matriz de 21696 x 21696 */
 #define NX 21696
 #define NY 21696
+#define N_KERNEL
+
+int kernel[N_KERNEL][N_KERNEL] = {{-1, -1, -1},
+                                  {-1, 8, -1},
+                                  {-1, -1, -1}};
 
 float data_in[NX][NY];
+float data_out[NX][NY];
+
+void convolute (float array[][], float result[][]);
 
 int main()
 {
     int ncid, varid;
     int retval;
+
+    for (int i=0; i<NX; i++){
+        for (int j=0; j<NY; j++){
+            data_out[i][j] = 0;
+        }
+    }
+
+    //float ** data_in;
+
+    //data_in = calloc(NX*NY, sizeof(float *));
 
     if ((retval = nc_open(FILE_NAME, NC_NOWRITE, &ncid)))
         ERR(retval);
@@ -33,10 +51,7 @@ int main()
         ERR(retval);
 
     /* el desarrollo acá */
-    for (int i=0; i< NX; i++){
-        for (int j=0; j<NY; j++)
-            printf("%f", data_in[i][j]);
-    }
+    convolute(data_in);
 
 
     /* Se cierra el archivo y liberan los recursos*/
@@ -45,3 +60,22 @@ int main()
 
     return 0;
 }
+
+void convolute (float input[][], float result[][])
+{
+    for (int fil_img=0; fil_img<NX; fil_img++){ //Iterar sobre filas de la imagen
+        for (int col_img=0; col_img<NY; col_img++){ //Iterar sobre columnas de la imagen
+            for (int fil_kernel=0; fil_kernel<N_KERNEL; fil_kernel++){ //Iterar sobre filas del kernel
+                for (int col_kernel=0; col_kernel<N_KERNEL; col_kernel++){ //Iterar sobre columnas del kernel
+
+                    int fil_img_aux = fil_img + fil_kernel;
+                    int col_img_aux = col_img + col_kernel;
+
+                    data_out[fil_img][col_img] = input[fil_img_aux][col_img_aux] * kernel[fil_kernel][col_kernel];
+                }//col_kernel
+            }//fil_kernel
+        }//col_img
+    }//fil_img
+
+}
+
--- TP2/src/main.py
+++ TP2/src/main.py
@@ -1,8 +1,29 @@
 from netCDF4 import Dataset
 import numpy as np
+from scipy.signal import convolve2d
+from matplotlib import pyplot as plt
 
 filename = "../includes/OR_ABI-L2-CMIPF-M6C02_G16_s20191011800206_e20191011809514_c20191011809591.nc"
+data_in = Dataset(filename, mode="r")
 
-fd = Dataset(filename, mode="r")
+matrix = data_in.variables['CMI'][:]
 
-fd.close()
+np.ma.set_fill_value(matrix, 0)
+matrix = matrix.filled()
+border_kernel = np.array ([
+    [-1,-1,-1],
+    [-1,8,-1],
+    [-1,-1,-1],
+    ])
+matrix = matrix[::3,::3]
+convoluted_img = convolve2d(matrix, border_kernel, mode='valid')
+import pdb; pdb.set_trace()
+
+#plt.imshow(matrix, cmap='gray')
+#plt.show()
+#plt.figure()
+plt.imshow(convoluted_img, cmap='gray', vmin=0, vmax=1)
+plt.show()
+
+data_in.close()
+exit()
