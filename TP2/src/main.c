dcdb601cfc670e9809dd563274c3b11a72493720
JuanSoriano <ju_soriano88@hotmail.com>
2019-05-12 17:24:34 -0300

    tp2 avances

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <netcdf.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /* Handle errors by printing an error message and exiting with a
  * non-zero status. */
@@ -9,31 +11,48 @@
 
 /* nombre del archivo a leer */
 #define FILE_NAME "../includes/OR_ABI-L2-CMIPF-M6C02_G16_s20191011800206_e20191011809514_c20191011809591.nc"
+#define FILE_OUT_NC "../includes/convolved_out.nc"
+#define FILE_OUT_BIN "../includes/convolved_out.bin"
 
 /* Lectura de una matriz de 21696 x 21696 */
 #define NX 21696
 #define NY 21696
-#define N_KERNEL
+#define N_KERNEL 3
 
-int kernel[N_KERNEL][N_KERNEL] = {{-1, -1, -1},
-                                  {-1, 8, -1},
-                                  {-1, -1, -1}};
+//float data_in[NX][NY];
+//float data_out[NX][NY];
 
-float data_in[NX][NY];
-float data_out[NX][NY];
+void convolve (float *data_in, float kernel[][N_KERNEL], float *data_out);
+void write_to_nc(float *data_out);
+void write_to_bin(float *data_out);
 
-void convolute (float array[][], float result[][]);
 
 int main()
 {
-    int ncid, varid;
-    int retval;
+    float *data_out = (float*)calloc((NX-N_KERNEL+1) * (NY-N_KERNEL+1), sizeof(float));
+    //float *data_out = (float*)calloc(NX*NY, sizeof(float));
+    float *data_in = (float*)calloc(NX*NY, sizeof(float));
 
-    for (int i=0; i<NX; i++){
+    float kernel [N_KERNEL][N_KERNEL] = {{-1, -1, -1},
+                                         {-1, 8, -1},
+                                         {-1, -1, -1}};
+
+    int ncid, varid, retval;
+
+  /*  for (int i=0; i<NX; i++){
         for (int j=0; j<NY; j++){
-            data_out[i][j] = 0;
+            data_out[i+j] = (float) 0;
         }
-    }
+    }*/
+
+    size_t start[2] = {0};
+    size_t count[2] = {0};
+
+    count[0] = NX;
+    count[1] = NY;
+
+    start[0] = 0;
+    start[1] = 0;
 
     //float ** data_in;
 
@@ -47,35 +66,93 @@ int main()
         ERR(retval);
 
     /* Leemos la matriz. */
-    if ((retval = nc_get_var_float(ncid, varid, &data_in[0][0])))
+    if ((retval = nc_get_vara_float(ncid, varid, start, count, data_in)))
         ERR(retval);
-
-    /* el desarrollo acá */
-    convolute(data_in);
-
 
     /* Se cierra el archivo y liberan los recursos*/
     if ((retval = nc_close(ncid)))
-        ERR(retval);
+    ERR(retval);
+
+    /* el desarrollo acá */
+    convolve(data_in, kernel, data_out);
+
+    free(data_in);
+/*    for (int i=0+NX/2; i<NX/2+100; i++) {
+        for (int j=0+NY/2; j<NX/2+100;j++){
+            printf("%f ", data_in[i*NX+j]);
+            //printf("%f ", data_out[i*NX+j]);
+        }
+        printf("\n");
+    }*/
+    //write_to_nc(data_out);
+    write_to_bin(data_out);
+    free(data_out);
+
+
+
 
     return 0;
 }
 
-void convolute (float input[][], float result[][])
+void convolve(float *data_in, float kernel[][N_KERNEL], float *data_out)
 {
-    for (int fil_img=0; fil_img<NX; fil_img++){ //Iterar sobre filas de la imagen
-        for (int col_img=0; col_img<NY; col_img++){ //Iterar sobre columnas de la imagen
+    for (int fil_img=0; fil_img<(NX-N_KERNEL+1); fil_img++){ //Iterar sobre filas de la imagen
+        for (int col_img=0; col_img<(NY-N_KERNEL+1); col_img++){ //Iterar sobre columnas de la imagen
             for (int fil_kernel=0; fil_kernel<N_KERNEL; fil_kernel++){ //Iterar sobre filas del kernel
                 for (int col_kernel=0; col_kernel<N_KERNEL; col_kernel++){ //Iterar sobre columnas del kernel
-
-                    int fil_img_aux = fil_img + fil_kernel;
-                    int col_img_aux = col_img + col_kernel;
-
-                    data_out[fil_img][col_img] = input[fil_img_aux][col_img_aux] * kernel[fil_kernel][col_kernel];
+                    data_out[fil_img*NX+col_img] += data_in[(fil_img+fil_kernel)*NX+(col_img+col_kernel)] * kernel[fil_kernel][col_kernel];
                 }//col_kernel
             }//fil_kernel
         }//col_img
     }//fil_img
+}
 
+void write_to_nc(float *data_out)
+{
+    int ncid, varid, retval;
+    size_t start[2] = {0};
+    start[0] = 0;
+    start[1] = 0;
+    size_t count[2] = {0};
+    count[0] = NX;
+    count[1] = NX;
+
+    /*if ((retval = nc_create(FILE_OUT, 0, &ncid))) {
+        ERR(retval);
+    }*/
+
+    if ((retval = nc_open(FILE_OUT_NC, NC_WRITE | NC_SHARE, &ncid)))
+        ERR(retval);
+
+    if ((retval = nc_inq_varid(ncid, "CMI", &varid))) {
+        ERR(retval);
+    }
+
+    if ((retval = nc_put_vara_float(ncid, varid, start, count, data_out))){
+        printf("retval:_%i", retval);
+        ERR(retval);
+    }
+
+    if ((retval = nc_close(ncid))) {
+        ERR(retval);
+    }
+}
+
+void write_to_bin(float *data_out)
+{
+    int fd;
+    if ((fd = open(FILE_OUT_BIN, O_WRONLY|O_CREAT|O_TRUNC, 0666)) <0)
+    {
+        printf("Error creando el file\n");
+        return;
+    }
+
+    if ((write(fd, data_out, (NX-N_KERNEL+1) * (NY-N_KERNEL+1) * sizeof(float)) < 0))
+    {
+        perror("ERROR escribiendo en el file");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
 }
 
--- TP2/src/main.py
+++ TP2/src/main.py
@@ -3,7 +3,11 @@ import numpy as np
 from scipy.signal import convolve2d
 from matplotlib import pyplot as plt
 
+NX=21696
+NY=21696
+NKERNEL=3
 filename = "../includes/OR_ABI-L2-CMIPF-M6C02_G16_s20191011800206_e20191011809514_c20191011809591.nc"
+convolve_filename = "../includes/convolved_out.bin"
 data_in = Dataset(filename, mode="r")
 
 matrix = data_in.variables['CMI'][:]
@@ -16,13 +20,11 @@ border_kernel = np.array ([
     [-1,-1,-1],
     ])
 matrix = matrix[::3,::3]
-convoluted_img = convolve2d(matrix, border_kernel, mode='valid')
-import pdb; pdb.set_trace()
 
-#plt.imshow(matrix, cmap='gray')
-#plt.show()
-#plt.figure()
-plt.imshow(convoluted_img, cmap='gray', vmin=0, vmax=1)
+#convoluted_img = convolve2d(matrix, border_kernel, mode='valid')
+#import pdb; pdb.set_trace()
+convolved_in_c = np.fromfile(convolve_filename,count=(NX-NKERNEL+1)*(NY-NKERNEL+1),dtype=np.float32,sep="").reshape(NX-NKERNEL+1,NY-NKERNEL+1)
+plt.imshow(convolved_in_c, cmap='gray', vmin=0, vmax=1.2)
 plt.show()
 
 data_in.close()
