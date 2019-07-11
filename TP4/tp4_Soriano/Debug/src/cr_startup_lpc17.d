bc3008e2c011c353e77cde79b75d13e93826391b
SorianoJuan <ju_soriano88@hotmail.com>
2019-07-10 21:53:21 -0300

    TP4 falta informe

---
+++ TP4/tp4_Soriano/Debug/src/cr_startup_lpc17.d
@@ -0,0 +1,4 @@
+src/cr_startup_lpc17.o src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
+ /home/torce/Facultad/SO2/so2/TP4/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h
+
+/home/torce/Facultad/SO2/so2/TP4/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h:
